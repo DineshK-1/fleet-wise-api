326af2019c8a0a2b00cb8f7c49b6326d129548be
Dinesh <dineshkumar.software@gmail.com>
2023-06-18 10:48:26 +0530

    removed dotenv

--- Database/sql.py
+++ Database/sql.py
@@ -2,10 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("POSTGRES_URL")
 
